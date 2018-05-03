0adbb66dbb6a3c87ae7b716353f309f5f8828638
Shehbaz Jaffer <shehbaz@cs.toronto.edu>
2018-05-03 18:00:41 -0500

    added parameters

--- microbench/workload.py
+++ microbench/workload.py
@@ -23,3 +23,47 @@
 #	X% of disk
 #	syncFreq (by default 100)
 #	this command would open, update (append or overwrite) existing file.
+
+import argparse
+import sys
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Generate Shell Commands for creating/reading and writing data')
+	subparsers = parser.add_subparsers(help='create|fillSpace|delete|update help')
+
+	# create
+
+	parser_create = subparsers.add_parser('create', help='creates files and directories in an empty file system')
+	parser_create.add_argument('--maxDirDepth',type=int,required=True)
+	parser_create.add_argument('--numFiles',type=int,required=True)
+	parser_create.add_argument('--minFileSize',type=str,default='4096', help=' (add KB, MB or GB. defaults to bytes)')	
+	parser_create.add_argument('--maxFileSize',type=str,default='1MB',help=' (add KB, MB or GB. defaults to bytes)')
+	parser_create.add_argument('--totalSpaceAllocation',type=int,help='Percentage of disk that needs to be filled')
+	parser_create.add_argument('--fileOnly',type=bool, default=False)
+	parser_create.add_argument('--dirOnly',type=bool, default=False)
+	parser_create.add_argument('--syncFreq',type=int,default=100)
+
+	# fillSpace (does not take numFiles)
+
+	parser_fillSpace = subparsers.add_parser('fillSpace', help='needs a file system that already has data')
+
+	parser_fillSpace.add_argument('--minFileSize',type=str,default='4096', help=' (add KB, MB or GB. defaults to bytes)')	
+	parser_fillSpace.add_argument('--maxFileSize',type=str,default='1MB',help=' (add KB, MB or GB. defaults to bytes)')
+	parser_fillSpace.add_argument('--syncFreq',type=int,required=True)
+
+	# delete (does not take minFileSize)
+	parser_delete = subparsers.add_parser('delete', help='needs a filesystem that already has data')
+	parser_delete.add_argument('--size',type=int,help='Percentage of disk that needs to be filled (in %)', required=True)
+
+	# update (does not take minFileSize, takes syncFreq)
+	parser_update = subparsers.add_parser('update', help='needs a filesystem that already has data')
+	parser_update.add_argument('--size',type=int,help='Percentage of disk that needs to be filled', required=True)
+
+#	parser.add_argument('--')
+	args = parser.parse_args()
+
+
+	print args
+#	print args.maxDirDepth
+#	print args.syncFreq
